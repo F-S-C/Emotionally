637923b071c798df5d8ef748966742cba144056c
Andrea Esposito <esposito_andrea99@hotmail.com>
2019-11-26 09:40:42 +0100

    Setup Jekyll (GitHub Pages)

--- docs/make.bat
+++ docs/make.bat
@@ -14,35 +14,35 @@ REM .PHONY: pdf html clean
 REM all: pdf clean
 
 :all
-set ALL=true
-goto :pdf
+    set ALL=true
+    goto :pdf
 
 :pdf
-pushd "%SOURCE_DIR%"
-pdflatex "%BASEFILENAME%.tex" -synctex=1 -interaction=batchmode --shell-escape
-biber "%BASEFILENAME%"
-pdflatex "%BASEFILENAME%.tex" -synctex=1 -interaction=batchmode --shell-escape
-pdflatex "%BASEFILENAME%.tex" -synctex=1 -interaction=batchmode --shell-escape
-xcopy "%BASEFILENAME%.pdf" .. /Y
-popd
-if "%ALL%"=="true" goto html
-goto end
+    pushd "%SOURCE_DIR%"
+    pdflatex "%BASEFILENAME%.tex" -synctex=1 -interaction=batchmode --shell-escape
+    biber "%BASEFILENAME%"
+    pdflatex "%BASEFILENAME%.tex" -synctex=1 -interaction=batchmode --shell-escape
+    pdflatex "%BASEFILENAME%.tex" -synctex=1 -interaction=batchmode --shell-escape
+    xcopy "%BASEFILENAME%.pdf" .. /Y
+    popd
+    if "%ALL%"=="true" goto html
+    goto end
 
 :html
-pushd "%SOURCE_DIR%"
-pandoc "%BASEFILENAME%.tex" -f latex -t html -s -o "%BASEFILENAME%.html"
-xcopy "%BASEFILENAME%.html" .. /Y
-popd
-if "%ALL%"=="true" goto clean
-goto end
+    pushd "%SOURCE_DIR%"
+    pandoc "%BASEFILENAME%.tex" -f latex -t html -s -o "%BASEFILENAME%.html" --template _template.html -V lang=it -V title="System Documentation" --table-of-contents
+    xcopy "%BASEFILENAME%.html" .. /Y
+    popd
+    if "%ALL%"=="true" goto clean
+    goto end
 
 :clean
-pushd "%SOURCE_DIR%"
-del /Q /F *.bcf *.run.xml *.aux *.glo *.idx *.log *.toc *.ist *.acn *.acr *.alg *.bbl *.blg *.dvi *.glg *.gls *.ilg *.ind *.lof *.lot *.maf *.mtc *.mtc1 *.out *.synctex.gz "*.synctex(busy)" *.thm
-popd
-goto end
+    pushd "%SOURCE_DIR%"
+    del /Q /F *.bcf *.run.xml *.aux *.glo *.idx *.log *.toc *.ist *.acn *.acr *.alg *.bbl *.blg *.dvi *.glg *.gls *.ilg *.ind *.lof *.lot *.maf *.mtc *.mtc1 *.out *.synctex.gz "*.synctex(busy)" *.thm
+    popd
+    goto end
 
 :error
-echo make: *** No rule to make target '%1%'. Stop.
+    echo make: *** No rule to make target '%1%'. Stop.
 
 :end
