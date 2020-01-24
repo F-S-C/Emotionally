95493e0b2088c827d1f87ffa1eda8e6d3df156b8
Andrea Esposito <esposito_andrea99@hotmail.com>
2020-01-24 10:23:07 +0100

    Rebuild della documentazione

--- make.bat
+++ make.bat
@@ -1,24 +1,32 @@
 @echo off
 
-if "%1"=="docs" goto docs
-if "%1"=="serve" goto serve
-goto error
+
+IF /I "%1"=="docs" GOTO docs
+IF /I "%1"=="serve" GOTO serve
+IF /I "%1"=="clean" GOTO clean
+IF /I "%1"=="dev-install" GOTO dev-install
+GOTO error
 
 :docs
-	pushd docs
-    call make.bat
-    popd
-    goto EOF
+	PUSHD docs && CALL make.bat && POPD
+	GOTO :EOF
 
 :serve
-	pushd src
-    php artisan serve
-    popd
-    goto EOF
+	PUSHD src && php artisan serve && POPD
+	GOTO :EOF
+
+:clean
+	PUSHD docs && CALL make.bat clean && POPD
+	GOTO :EOF
+
+:dev-install
+	PUSHD src && composer install && POPD
+	GOTO :EOF
 
 :error
-    if "%1"=="" (
-        echo make: *** No targets specified and no makefile found.  Stop.
-    ) else (
-        echo make: *** No rule to make target '%1%'. Stop.
+    IF "%1"=="" (
+        ECHO make: *** No targets specified and no makefile found.  Stop.
+    ) ELSE (
+        ECHO make: *** No rule to make target '%1%'. Stop.
     )
+    GOTO :EOF
