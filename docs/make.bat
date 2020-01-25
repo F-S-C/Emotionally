b9d895ac2d95afc1584e3b89a9fa3ef33ccc3446
Andrea Esposito <esposito_andrea99@hotmail.com>
2020-01-25 16:45:20 +0100

    Rebuild della documentazione

--- docs/make.bat
+++ docs/make.bat
@@ -27,13 +27,13 @@ GOTO error
 	PUSHD "%SOURCE_DIR%" && biber "%BASEFILENAME%" && POPD
 	PUSHD "%SOURCE_DIR%" && pdflatex -synctex=1 -interaction=batchmode --shell-escape "%BASEFILENAME%.tex" && POPD
 	PUSHD "%SOURCE_DIR%" && pdflatex -synctex=1 -interaction=batchmode --shell-escape "%BASEFILENAME%.tex" && POPD
-	PUSHD "%SOURCE_DIR%" && XCOPY "%BASEFILENAME%.pdf" .. && POPD
+	PUSHD "%SOURCE_DIR%" && XCOPY /Y "%BASEFILENAME%.pdf" .. && POPD
 	GOTO :EOF
 
 :html
 	CALL make.bat pre
 	PUSHD "%SOURCE_DIR%" && pandoc "%BASEFILENAME%.tex" -f latex -t html -s -o "%BASEFILENAME%.html" --template _template.html -V lang=it -V title="System Documentation" --table-of-contents && POPD
-	PUSHD "%SOURCE_DIR%" && XCOPY "%BASEFILENAME%.html" .. && POPD
+	PUSHD "%SOURCE_DIR%" && XCOPY /Y "%BASEFILENAME%.html" .. && POPD
 	GOTO :EOF
 
 :clean
--- docs/src/helper.py
+++ docs/src/helper.py
@@ -38,7 +38,14 @@ def scenarios():
         return
 
     data = sorted(data, key=lambda i: i['id'])
-    content = ''
+    content = r'''% Copyright (c)  2019  FSC.
+% Permission is granted to copy, distribute and/or modify this document
+% under the terms of the GNU Free Documentation License, Version 1.3
+% or any later version published by the Free Software Foundation;
+% with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
+% A copy of the license is included in the section entitled "GNU
+% Free Documentation License".
+'''
     for scenario in data:
         content += f'''\\begin{{table}}[H]
 	\\centering
