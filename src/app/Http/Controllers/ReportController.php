4ae50f14c49143c1471535d46e5dbbd08623ffd7
Davidedes <44806393+Davidedes@users.noreply.github.com>
2020-02-08 19:56:01 +0100

    eliminati dei metodi

--- src/app/Http/Controllers/ReportController.php
+++ src/app/Http/Controllers/ReportController.php
@@ -26,31 +26,6 @@ use Illuminate\Http\Request;
 
 class ReportController extends Controller
 {
-
-
-    /**
-     * Get the view with all the current user's projects.
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View The dashboard view.
-     */
-    public function getDashboard()
-    {
-        return view('home')->with('projects', $this->getAllProjects(User::first())); //auth()->user()
-    }
-
-    /**
-     * Get a list of all the projects of a user. This includes the projects of
-     * which a user is owner and those that were shared with the same user.
-     * @param User $user The user "owner" of the projects.
-     * @return Project[]|\Illuminate\Database\Eloquent\Collection|mixed The projects.
-     */
-    private function getAllProjects(User $user)
-    {
-        $owned_projects = $user->projects->where('father_id', null);
-        $shared_projects = $user->permissions;
-
-        return $owned_projects->merge($shared_projects);
-    }
-
     /**
      * This function get and rename a video
      * @param Video $video
