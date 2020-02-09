83d7af2ebe42f9cd93350d6eee643925409a5bc5
Andrea Esposito <esposito_andrea99@hotmail.com>
2020-02-10 00:51:11 +0100

    Creazione della vista ad albero nella sidebar

--- src/resources/views/layouts/sidebar.blade.php
+++ src/resources/views/layouts/sidebar.blade.php
@@ -1,14 +1,23 @@
-{{--
-    @todo Define the sidebar layout
-    @body A layout to be used with a sidebar must be defined. It must extend the layout 'master'.
---}}
 @extends('layouts.master')
+
+@section('head')
+    @parent
+    <style>
+        #main {
+            padding: 15px;
+            width: 100%;
+            min-height: 100vh;
+            transition: all 0.3s;
+        }
+    </style>
+@endsection
 
 @section('body')
     <div class="wrapper">
         <nav class="sidebar el-8dp" id="main-navigation" aria-label="Sidebar">
             <div class="sidebar-header">
-                <a class="sidebar-brand text-center w-100" style="text-decoration: none;" href="{{route('system.home')}}">
+                <a class="sidebar-brand text-center w-100" style="text-decoration: none;"
+                   href="{{route('system.home')}}">
                     <img src="{{asset('/logo.png')}}" width="64"
                          height="64"
                          class="d-inline-block d-md-inline-block align-center mx-auto" alt="Emotionally's logo">
@@ -20,31 +29,19 @@
 
             <ul class="nav flex-column">
                 <li class="nav-item active">
-                    <div class="nav-link collapse-button-container">
-                        <a type="button" class="collapse-button" data-toggle="collapse" href="#projects-container" role="button" aria-expanded="false" aria-controls="projects-container"></a>
-                        <a href="{{route('system.home')}}">Projects</a>
+                    {{-- TODO: Use current logged user --}}
+                    <div class="btn-group collapse-button-container">
+                        <a type="button" class="nav-link collapse-button d-none d-md-block" data-toggle="collapse"
+                           href="#projects-container"
+                           role="button" aria-expanded="false" aria-controls="projects-container"></a>
+                        <a class="nav-link text-center text-md-left px-0" href="{{route('system.home')}}">
+                            <span aria-hidden="true" class="fas fa-home mr-1 text-md-center"></span>
+                            <span class="d-none d-md-inline">Projects</span>
+                        </a>
                     </div>
-                    <ul class="collapse el-3dp nav flex-column" id="projects-container">
-                        <li class="nav-item">
-                            <div class="nav-link collapse-button-container">
-                                <a type="button" class="collapse-button" data-toggle="collapse" href="#collapseExample2" role="button" aria-expanded="false" aria-controls="collapseExample"></a>
-                                <a href="#">Projects</a>
-                            </div>
-                            <ul class="collapse el-3dp nav flex-column" id="collapseExample2">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">About</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">About</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
-                        </li>
+                    <ul class="collapse el-3dp nav flex-column flex-nowrap" id="projects-container">
+                        {{-- TODO: Use current logged user --}}
+                        @each('partials.project-tree-view', \Emotionally\User::first()->projects->where('father_id', null), 'main_project')
                     </ul>
                 </li>
                 <li class="nav-item">
@@ -63,24 +60,25 @@
         <div class="content sidebar-content">
             <nav class="navbar navbar-expand-lg navbar-dark el-0dp" style="padding: 20px 30px;" aria-label="navbar">
                 <div class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2 rounded-pill" type="search" placeholder="Search" aria-label="Search" id="search-bar">
+                    <input class="form-control mr-sm-2 rounded-pill" type="search" placeholder="Search"
+                           aria-label="Search" id="search-bar">
                 </div>
-{{--                <button class="navbar-toggler" type="button" data-toggle="collapse"--}}
-{{--                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"--}}
-{{--                        aria-expanded="false" aria-label="Toggle navigation">--}}
-{{--                    <span class="navbar-toggler-icon"></span>--}}
-{{--                </button>--}}
+                {{--                <button class="navbar-toggler" type="button" data-toggle="collapse"--}}
+                {{--                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"--}}
+                {{--                        aria-expanded="false" aria-label="Toggle navigation">--}}
+                {{--                    <span class="navbar-toggler-icon"></span>--}}
+                {{--                </button>--}}
 
-{{--                <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
-{{--                    <ul class="navbar-nav mr-auto">--}}
-{{--                        <li class="nav-item active">--}}
-{{--                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>--}}
-{{--                        </li>--}}
-{{--                        <li class="nav-item">--}}
-{{--                            <a class="nav-link" href="#">Link</a>--}}
-{{--                        </li>--}}
-{{--                    </ul>--}}
-{{--                </div>--}}
+                {{--                <div class="collapse navbar-collapse" id="navbarSupportedContent">--}}
+                {{--                    <ul class="navbar-nav mr-auto">--}}
+                {{--                        <li class="nav-item active">--}}
+                {{--                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>--}}
+                {{--                        </li>--}}
+                {{--                        <li class="nav-item">--}}
+                {{--                            <a class="nav-link" href="#">Link</a>--}}
+                {{--                        </li>--}}
+                {{--                    </ul>--}}
+                {{--                </div>--}}
             </nav>
             <main id="main">
                 @yield('content')
