1cedaa4191da3f4842e985bd373e76d5738785a5
Andrea Esposito <esposito_andrea99@hotmail.com>
2020-02-09 21:16:48 +0100

    Aggiunta funzionalità per il calcolo dell'emozione modale

--- src/app/Http/Controllers/ReportController.php
+++ src/app/Http/Controllers/ReportController.php
@@ -21,48 +21,115 @@ namespace Emotionally\Http\Controllers;
 
 class ReportController extends Controller
 {
+    private const JOY_KEY = "joy";
+    private const SADNESS_KEY = "sadness";
+    private const DISGUST_KEY = "disgust";
+    private const CONTEMPT_KEY = "contempt";
+    private const ANGER_KEY = "anger";
+    private const FEAR_KEY = "fear";
+    private const SURPRISE_KEY = "surprise";
+    private const VALENCE_KEY = "valence";
+    private const ENGAGEMENT_KEY = "engagement";
+    private const TIMESTAMP_KEY = "Timestamp";
+    private const SMILE_KEY = "smile";
+    private const INNER_BROW_RAISE_KEY = "innerBrowRaise";
+    private const BROW_RAISE_KEY = "browRaise";
+    private const BROW_FURROW_KEY = "browFurrow";
+    private const NOSE_WRINKLE_KEY = "noseWrinkle";
+    private const UPPER_LIP_RAISE_KEY = "upperLipRaise";
+    private const LIP_CORNER_DEPRESSOR_KEY = "lipCornerDepressor";
+    private const CHIN_RAISE_KEY = "chinRaise";
+    private const LIP_PUCKER_KEY = "lipPucker";
+    private const LIP_PRESS_KEY = "lipPress";
+    private const LIP_SUCK_KEY = "lipSuck";
+    private const MOUTH_OPEN_KEY = "mouthOpen";
+    private const SMIRK_KEY = "smirk";
+    private const EYE_CLOSURE_KEY = "eyeClosure";
+    private const ATTENTION_KEY = "attention";
+    private const LID_TIGHTEN_KEY = "lidTighten";
+    private const JAW_DROP_KEY = "jawDrop";
+    private const DIMPLER_KEY = "dimpler";
+    private const EYE_WIDEN_KEY = "eyeWiden";
+    private const CHEEK_RAISE_KEY = "cheekRaise";
+    private const LIP_STRETCH_KEY = "lipStretch";
+
     /**
-     * Get the average emotion from a report.
+     * Get the average report from a list of reports.
      * @param string|array $json The report
      * @return array
      */
-    public function average($json)
+    public static function average($json)
     {
         if (is_string($json)) {
-            $json = json_decode($json);
+            $json = json_decode($json, true);
         } else if (!is_array($json)) {
             throw new \InvalidArgumentException("The input isn't a string or a JSON");
         }
 
+        $averageReport = [
+            self::JOY_KEY => 0,
+            self::SADNESS_KEY => 0,
+            self::DISGUST_KEY => 0,
+            self::CONTEMPT_KEY => 0,
+            self::ANGER_KEY => 0,
+            self::FEAR_KEY => 0,
+            self::SURPRISE_KEY => 0,
+            self::VALENCE_KEY => 0,
+            self::ENGAGEMENT_KEY => 0,
+            self::TIMESTAMP_KEY => 0,
+            self::SMILE_KEY => 0,
+            self::INNER_BROW_RAISE_KEY => 0,
+            self::BROW_RAISE_KEY => 0,
+            self::BROW_FURROW_KEY => 0,
+            self::NOSE_WRINKLE_KEY => 0,
+            self::UPPER_LIP_RAISE_KEY => 0,
+            self::LIP_CORNER_DEPRESSOR_KEY => 0,
+            self::CHIN_RAISE_KEY => 0,
+            self::LIP_PUCKER_KEY => 0,
+            self::LIP_PRESS_KEY => 0,
+            self::LIP_SUCK_KEY => 0,
+            self::MOUTH_OPEN_KEY => 0,
+            self::SMIRK_KEY => 0,
+            self::EYE_CLOSURE_KEY => 0,
+            self::ATTENTION_KEY => 0,
+            self::LID_TIGHTEN_KEY => 0,
+            self::JAW_DROP_KEY => 0,
+            self::DIMPLER_KEY => 0,
+            self::EYE_WIDEN_KEY => 0,
+            self::CHEEK_RAISE_KEY => 0,
+            self::LIP_STRETCH_KEY => 0
+        ];
+
         $iterated = 0;
-        $a = 0;
-        $b = 0;
-        $c = 0;
-        $d = 0;
-        $e = 0;
-        $f = 0;
-        $g = 0;
         foreach ($json as $row) {
-            $a = $row['a'] + $a;
-            $b = $row['a'] + $b;
-            $c = $row['a'] + $c;
-            $d = $row['a'] + $d;
-            $e = $row['a'] + $e;
-            $f = $row['a'] + $f;
-            $g = $row['a'] + $g;
+            foreach ($averageReport as $key => &$item) {
+                $item += $row[$key];
+            }
             $iterated++;
         }
-        $averageReport = [
-            'a' => $a / $iterated,
-            'b' => $b / $iterated,
-            'c' => $c / $iterated,
-            'd' => $d / $iterated,
-            'e' => $e / $iterated,
-            'f' => $f / $iterated,
-            'g' => $g / $iterated
-        ];
+
+        foreach ($averageReport as &$value) {
+            $value /= $iterated;
+        }
+
         return $averageReport;
     }
 
-
+    public static function highestEmotion($json)
+    {
+        $averageReport = [
+            self::JOY_KEY => 0,
+            self::SADNESS_KEY => 0,
+            self::DISGUST_KEY => 0,
+            self::CONTEMPT_KEY => 0,
+            self::ANGER_KEY => 0,
+            self::FEAR_KEY => 0,
+            self::SURPRISE_KEY => 0
+        ];
+        $totalAverageReport = self::average($json);
+        foreach ($averageReport as $key => &$value) {
+            $value = $totalAverageReport[$key];
+        }
+        return array_keys($averageReport, max($averageReport))[0];
+    }
 }
