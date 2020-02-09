39823fbd82ffce8bd69bb73ccd7251af33761bdf
Andrea Esposito <esposito_andrea99@hotmail.com>
2020-02-09 14:35:15 +0100

    Aggiunto layout per le pagine 'di sistema'

--- src/resources/views/home.blade.php
+++ src/resources/views/home.blade.php
@@ -1,65 +1,57 @@
-@extends('layouts.sidebar')
+@extends('layouts.system')
 
 @section('title', trans('dashboard.dashboard'))
 
-@section('content')
-    @parent
+@section('breadcrumbs')
+    <li class="breadcrumb-item" aria-current="page"><span class="fas fa-home" aria-hidden="true"></span>
+        @lang('dashboard.home')
+    </li>
+    <li class="breadcrumb-item active" aria-current="page">
+        @lang('dashboard.dashboard')
+    </li>
+@endsection
 
-    <section class="container-fluid" id="content">
-        <header>
-            <h1 class="d-block d-md-inline">@lang('dashboard.home')</h1>
-            <nav id="breadcrumbs" class="breadcrumb-container" aria-label="breadcrumbs">
-                <ol class="breadcrumb bg-transparent">
-                    <li class="breadcrumb-item" aria-current="page"><span class="fas fa-home" aria-hidden="true"></span>
-                        @lang('dashboard.home')
-                    </li>
-                    <li class="breadcrumb-item active" aria-current="page">
-                        @lang('dashboard.dashboard')
-                    </li>
-                </ol>
-            </nav>
-        </header>
-        <div class="table-responsive mt-3">
-            <table id="project-table" class="display w-100 table table-striped table-borderless">
-                <caption class="sr-only">@lang('dashboard.your_projects')</caption>
-                <thead class="text-uppercase">
+@section('inner-content')
+    <div class="table-responsive mt-3">
+        <table id="project-table" class="display w-100 table table-striped table-borderless">
+            <caption class="sr-only">@lang('dashboard.your_projects')</caption>
+            <thead class="text-uppercase">
+            <tr>
+                <th scope="col">@lang('dashboard.name')</th>
+                <th scope="col" class="text-center">@lang('dashboard.created_at')</th>
+                <th scope="col" class="text-center">@lang('dashboard.updated_at')</th>
+                <th scope="col" class="text-center">@lang('dashboard.videos')</th>
+                <th scope="col" class="text-center">@lang('dashboard.subprojects')</th>
+                <th scope="col" class="text-center">@lang('dashboard.average_emotion')</th>
+                <th scope="col"><span class="sr-only">@lang('dashboard.go_to_report')</span></th>
+                <th scope="col"><span class="sr-only">@lang('dashboard.more')</span></th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($projects as $project)
                 <tr>
-                    <th scope="col">@lang('dashboard.name')</th>
-                    <th scope="col" class="text-center">@lang('dashboard.created_at')</th>
-                    <th scope="col" class="text-center">@lang('dashboard.updated_at')</th>
-                    <th scope="col" class="text-center">@lang('dashboard.videos')</th>
-                    <th scope="col" class="text-center">@lang('dashboard.subprojects')</th>
-                    <th scope="col" class="text-center">@lang('dashboard.average_emotion')</th>
-                    <th scope="col"><span class="sr-only">@lang('dashboard.go_to_report')</span></th>
-                    <th scope="col"><span class="sr-only">@lang('dashboard.more')</span></th>
+                    <td>{{$project['name']}}</td>
+                    <td class="text-center">{{date('d/m/Y',strtotime($project->created_at))}}</td>
+                    <td class="text-center">{{date('d/m/Y', strtotime($project->updated_at))}}</td>
+                    <td class="text-center">{{$project->number_of_videos}}</td>
+                    <td class="text-center">{{$project->number_of_subprojects}}</td>
+                    <td class="text-center">{{$project->average_emotion}}</td>
+                    <td class="text-center">
+                        <a href="#" class="btn btn-md-text"
+                           aria-label="@lang('dashboard.go_to_project_report', ['name'=>$project->name])">@lang('dashboard.report')</a>
+                    </td>
+                    <td>
+                        <button class="btn btn-outline-light border-0 rounded-circle">
+                            <span class="fas fa-ellipsis-v" aria-hidden="true"
+                                  title="@lang('dashboard.more_options')"></span>
+                            <span class="sr-only">@lang('dashboard.more_options')</span>
+                        </button>
+                    </td>
                 </tr>
-                </thead>
-                <tbody>
-                @foreach($projects as $project)
-                    <tr>
-                        <td>{{$project['name']}}</td>
-                        <td class="text-center">{{date('d/m/Y',strtotime($project->created_at))}}</td>
-                        <td class="text-center">{{date('d/m/Y', strtotime($project->updated_at))}}</td>
-                        <td class="text-center">{{$project->number_of_videos}}</td>
-                        <td class="text-center">{{$project->number_of_subprojects}}</td>
-                        <td class="text-center">{{$project->average_emotion}}</td>
-                        <td class="text-center">
-                            <a href="#" class="btn btn-md-text"
-                               aria-label="@lang('dashboard.go_to_project_report', ['name'=>$project->name])">@lang('dashboard.report')</a>
-                        </td>
-                        <td>
-                            <button class="btn btn-outline-light border-0 rounded-circle">
-                                <span class="fas fa-ellipsis-v" aria-hidden="true" title="@lang('dashboard.more_options')"></span>
-                                <span class="sr-only">@lang('dashboard.more_options')</span>
-                            </button>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </div>
-
-    </section>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
 
 @section('scripts')
