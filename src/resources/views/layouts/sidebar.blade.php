f15c92208da55958832c1608e8628935c72ca2e3
Ax3lFernus <a.annese99@gmail.com>
2020-02-10 20:07:12 +0100

    Modificato link di logout nella sidebar

--- src/resources/views/layouts/sidebar.blade.php
+++ src/resources/views/layouts/sidebar.blade.php
@@ -66,8 +66,11 @@
                         <span class="d-none d-md-inline">Contact</span>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('logout') }}">Logout</a>
+                <li class="nav-item text-center text-md-left">
+                    <a class="nav-link" href="{{ route('logout') }}">
+                        <span aria-hidden="true" class="fas fa-sign-out-alt mr-0 mr-md-1 text-md-center"></span>
+                        <span class="d-none d-md-inline">Logout</span>
+                    </a>
                 </li>
             </ul>
 
