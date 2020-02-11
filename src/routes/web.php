878ee2b8c0c64e9614606425a0ce02061d62d4fa
Andrea Esposito <esposito_andrea99@hotmail.com>
2020-02-11 16:56:58 +0100

    Modifiche minori

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,19 +16,21 @@
 
 Route::view('/', 'landing')->name('landing');
 
-Route::name('system.')->middleware('auth')->middleware('verified')->prefix('system')->group(function () {
-    Route::get('/', 'ProjectController@getDashboard')->name('home');
-    Route::redirect('/home', '/system/');
+Route::name('system.')
+    ->middleware('auth')
+    ->middleware('verified')
+    ->prefix('system')
+    ->group(function () {
+        Route::get('/', 'ProjectController@getDashboard')->name('home');
+        Route::redirect('/home', '/system/');
 
-    Route::get('/project/{id}', 'ProjectController@getProjectDetails')->name('project-details');
-});
+        Route::get('/project/{id}', 'ProjectController@getProjectDetails')->name('project-details');
+    });
 
 Auth::routes(/*['verify' => true]*/);
 
-Route::get('/logout', function(){
+Route::get('/logout', function () {
     Auth::logout();
 
     return redirect()->route('landing');
 })->name('logout');
-
-Route::get('/home', 'HomeController@index')->name('home');
