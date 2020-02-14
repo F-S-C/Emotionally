39b7472d0892651582b29244b747bb3e4a24be03
Ax3lFernus <a.annese99@gmail.com>
2020-02-14 17:55:41 +0100

    Corretto errore grafico nella visualizzazione html dei report

--- src/resources/views/layout-file.blade.php
+++ src/resources/views/layout-file.blade.php
@@ -1,207 +1,191 @@
 @extends('layouts.blank')
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="{{mix('/js/app.js')}}" type="text/javascript"></script>
-    <title>{{$video->name}}</title>
 
-</head>
-<body>
-    <div class="container">
-        <div class="container-fluid">
-            <div class="card-deck">
-                <div class="card el-0dp">
-                    <div class="card-body">
-                        <h3 class="card-title">Spider Chart</h3>
-                        <div class="smaller-charts">
-                            <canvas id="radar"></canvas>
-                        </div>
-                    </div>
-                </div>
-                <div class="card el-0dp">
-                    <div class="card-body">
-                        <h3 class="card-title">Bar Chart</h3>
-                        <div class="smaller-charts">
-                            <canvas id="bar"></canvas>
-                        </div>
-                    </div>
+@section('body')
+    <div class="container w-75">
+        <div class="row">
+            <div class="col-6">
+                <h3>Spider Chart</h3>
+                <div class="smaller-charts">
+                    <canvas id="radar"></canvas>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-12">
-                    <h3>Line Chart</h3>
-                    <div class="bigger-charts">
-                        <canvas id="line"></canvas>
-                    </div>
+            <div class="col-6">
+                <h3>Bar Chart</h3>
+                <div class="smaller-charts">
+                    <canvas id="bar"></canvas>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h3>Line Chart</h3>
+                <div class="bigger-charts">
+                    <canvas id="line"></canvas>
                 </div>
             </div>
         </div>
     </div>
+@endsection
 
-    @section('scripts')
-        <script>
-            (function () {
-                $(document).ready(function () {
-                    let averageReport = @json($video->average_report);
-                    let fullReport = @json(\Emotionally\Http\Controllers\ReportController::getEmotionValues($video->report));
+@section('scripts')
+    <script>
+        (function () {
+            $(document).ready(function () {
+                let averageReport = @json($video->average_report);
+                let fullReport = @json(\Emotionally\Http\Controllers\ReportController::getEmotionValues($video->report));
 
-                    let radar = document.getElementById("radar").getContext("2d");
-                    let line = document.getElementById("line").getContext("2d");
-                    let bar = document.getElementById("bar").getContext("2d");
+                let radar = document.getElementById("radar").getContext("2d");
+                let line = document.getElementById("line").getContext("2d");
+                let bar = document.getElementById("bar").getContext("2d");
 
-                    /**
-                     * Create a new radar chart.
-                     */
-                    new Chart(radar, {
-                        type: 'radar',
-                        data: {
-                            labels: Object.keys(averageReport).map(s => s.charAt(0).toUpperCase() + s.slice(1)),
-                            datasets: [
-                                {
-                                    label: 'Emotions',
-                                    data: Object.keys(averageReport).map(el => averageReport[el]),
-                                    fill: true,
-                                    backgroundColor: 'rgba(255, 152, 0, 0.3)',
-                                    borderColor: 'rgba(255, 152, 0, 0.7)',
-                                    pointBackgroundColor: 'rgba(255, 152, 0, 1)',
-                                    pointBorderColor: 'rgba(255, 255, 255, 0.9)'
-                                }
-                            ]
+                /**
+                 * Create a new radar chart.
+                 */
+                new Chart(radar, {
+                    type: 'radar',
+                    data: {
+                        labels: Object.keys(averageReport).map(s => s.charAt(0).toUpperCase() + s.slice(1)),
+                        datasets: [
+                            {
+                                label: 'Emotions',
+                                data: Object.keys(averageReport).map(el => averageReport[el]),
+                                fill: true,
+                                backgroundColor: 'rgba(255, 152, 0, 0.3)',
+                                borderColor: 'rgba(255, 152, 0, 0.7)',
+                                pointBackgroundColor: 'rgba(255, 152, 0, 1)',
+                                pointBorderColor: 'rgba(255, 255, 255, 0.9)'
+                            }
+                        ]
+                    },
+                    options: {
+                        scale: {
+                            angleLines: {
+                                color: 'rgba(0, 0, 0, 0.5)'
+                            },
+                            gridLines: {
+                                color: 'rgba(0, 0, 0, 0.5)'
+                            },
+                            pointLabels: {
+                                fontColor: 'rgba(0, 0, 0, 0.7)',
+                                fontSize: 12
+                            },
+                            ticks: {
+                                showLabelBackdrop: false,
+                                fontColor: 'rgba(0, 0, 0, 0.7)'
+                            }
                         },
-                        options: {
-                            scale: {
-                                angleLines: {
-                                    color: 'rgba(0, 0, 0, 0.5)'
-                                },
+                        legend: {
+                            labels: {
+                                fontColor: '#000'
+                            }
+                        },
+                        maintainAspectRatio: false
+                    }
+                });
+
+                /**
+                 * Create a new bar chart.
+                 */
+                new Chart(bar, {
+                    type: 'bar',
+                    data: {
+                        labels: Object.keys(averageReport).map(s => s.charAt(0).toUpperCase() + s.slice(1)),
+                        datasets: [
+                            {
+                                label: 'Emotions',
+                                data: Object.keys(averageReport).map(el => averageReport[el]),
+                                fill: false,
+                                barPercentage: 0.25,
+                                backgroundColor: 'rgba(255, 152, 0, 1)',
+                                hoverBackgroundColor: 'rgba(255, 152, 0, 0.7)'
+                            }
+                        ]
+                    },
+                    options: {
+                        scales: {
+                            xAxes: [{
                                 gridLines: {
-                                    color: 'rgba(0, 0, 0, 0.5)'
-                                },
-                                pointLabels: {
-                                    fontColor: 'rgba(0, 0, 0, 0.7)',
-                                    fontSize: 12
+                                    color: 'rgba(0, 0, 0, 0.2)',
+                                    zeroLineColor: 'rgba(0, 0, 0, 0.5)'
                                 },
                                 ticks: {
-                                    showLabelBackdrop: false,
-                                    fontColor: 'rgba(0, 0, 0, 0.7)'
-                                }
-                            },
-                            legend: {
-                                labels: {
                                     fontColor: '#000'
                                 }
-                            },
-                            maintainAspectRatio: false
-                        }
-                    });
-
-                    /**
-                     * Create a new bar chart.
-                     */
-                    new Chart(bar, {
-                        type: 'bar',
-                        data: {
-                            labels: Object.keys(averageReport).map(s => s.charAt(0).toUpperCase() + s.slice(1)),
-                            datasets: [
-                                {
-                                    label: 'Emotions',
-                                    data: Object.keys(averageReport).map(el => averageReport[el]),
-                                    fill: false,
-                                    barPercentage: 0.25,
-                                    backgroundColor: 'rgba(255, 152, 0, 1)',
-                                    hoverBackgroundColor: 'rgba(255, 152, 0, 0.7)'
+                            }],
+                            yAxes: [{
+                                gridLines: {
+                                    color: 'rgba(0, 0, 0, 0.2)',
+                                    zeroLineColor: 'rgba(0, 0, 0, 0.5)'
+                                },
+                                ticks: {
+                                    fontColor: '#000'
                                 }
-                            ]
+                            }]
                         },
-                        options: {
-                            scales: {
-                                xAxes: [{
-                                    gridLines: {
-                                        color: 'rgba(0, 0, 0, 0.2)',
-                                        zeroLineColor: 'rgba(0, 0, 0, 0.5)'
-                                    },
-                                    ticks: {
-                                        fontColor: '#000'
-                                    }
-                                }],
-                                yAxes: [{
-                                    gridLines: {
-                                        color: 'rgba(0, 0, 0, 0.2)',
-                                        zeroLineColor: 'rgba(0, 0, 0, 0.5)'
-                                    },
-                                    ticks: {
-                                        fontColor: '#000'
-                                    }
-                                }]
-                            },
-                            legend: {
-                                labels: {
-                                    fontColor: '#000'
-                                }
-                            },
-                            maintainAspectRatio: false
-                        }
-                    });
-
-                    let colors = ['#FF9800', '#5BC0EB', '#E55934', '#084887', '#9BC53D', '#97959b', '#44AF69'];
-
-                    /**
-                     * Create a new line chart.
-                     */
-                    let lineChart = new Chart(line, {
-                        type: 'line',
-                        data: {
-                            labels: fullReport.map((_, i) => i), //TODO: Insert framerate as labels?
-                            datasets: Object.keys(fullReport[0]).map((key, i) => {
-                                return {
-                                    borderColor: colors[i],
-                                    pointBackgroundColor: colors[i],
-                                    pointBorderColor: colors[i],
-                                    label: key.charAt(0).toUpperCase() + key.slice(1),
-                                    data: fullReport.map(el => el[key]),
-                                    fill: false
-                                };
-                            })
+                        legend: {
+                            labels: {
+                                fontColor: '#000'
+                            }
                         },
-                        options: {
-                            scales: {
-                                xAxes: [{
-                                    gridLines: {
-                                        color: 'rgba(0, 0, 0, 0.2)',
-                                        zeroLineColor: 'rgba(0, 0, 0, 0.5)'
-                                    },
-                                    ticks: {
-                                        fontColor: '#000'
-                                    }
-                                }],
-                                yAxes: [{
-                                    gridLines: {
-                                        color: 'rgba(0, 0, 0, 0.2)',
-                                        zeroLineColor: 'rgba(0, 0, 0, 0.5)'
-                                    },
-                                    ticks: {
-                                        fontColor: '#000'
-                                    }
-                                }]
-                            },
-                            legend: {
-                                labels: {
+                        maintainAspectRatio: false
+                    }
+                });
+
+                let colors = ['#FF9800', '#5BC0EB', '#E55934', '#084887', '#9BC53D', '#97959b', '#44AF69'];
+
+                /**
+                 * Create a new line chart.
+                 */
+                let lineChart = new Chart(line, {
+                    type: 'line',
+                    data: {
+                        labels: fullReport.map((_, i) => i), //TODO: Insert framerate as labels?
+                        datasets: Object.keys(fullReport[0]).map((key, i) => {
+                            return {
+                                borderColor: colors[i],
+                                pointBackgroundColor: colors[i],
+                                pointBorderColor: colors[i],
+                                label: key.charAt(0).toUpperCase() + key.slice(1),
+                                data: fullReport.map(el => el[key]),
+                                fill: false
+                            };
+                        })
+                    },
+                    options: {
+                        scales: {
+                            xAxes: [{
+                                gridLines: {
+                                    color: 'rgba(0, 0, 0, 0.2)',
+                                    zeroLineColor: 'rgba(0, 0, 0, 0.5)'
+                                },
+                                ticks: {
                                     fontColor: '#000'
                                 }
-                            },
-                            maintainAspectRatio: false,
-                            plugins: {
-                                colorschemes: {
-                                    scheme: 'brewer.SetOne3'
+                            }],
+                            yAxes: [{
+                                gridLines: {
+                                    color: 'rgba(0, 0, 0, 0.2)',
+                                    zeroLineColor: 'rgba(0, 0, 0, 0.5)'
+                                },
+                                ticks: {
+                                    fontColor: '#000'
                                 }
+                            }]
+                        },
+                        legend: {
+                            labels: {
+                                fontColor: '#000'
+                            }
+                        },
+                        maintainAspectRatio: false,
+                        plugins: {
+                            colorschemes: {
+                                scheme: 'brewer.SetOne3'
                             }
                         }
-                    });
+                    }
                 });
-            })($);
-        </script>
-    @endsection
-</body>
-</html>
+            });
+        })($);
+    </script>
+@endsection
--- src/resources/views/layouts/blank.blade.php
+++ src/resources/views/layouts/blank.blade.php
@@ -4,6 +4,43 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+    <style>
+        @page {
+            size: A4;
+            margin: 0;
+        }
+
+        body{
+            background-color: white!important;
+            margin: 15px;
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6,
+        .h1,
+        .h2,
+        .h3,
+        .h4,
+        .h5,
+        .h6 {
+            font-weight: 800!important;
+            color: black!important;
+        }
+
+        .smaller-charts {
+            width: 30vw!important;
+        }
+
+        .bigger-charts {
+            width: 67vw!important;
+        }
+    </style>
     @yield('head', '')
 
     <title>@yield('title') | Emotionally</title>
