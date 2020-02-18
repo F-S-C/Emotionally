84cd65183c3d1967f4026ad019e2cead061f1a3e
Andrea Esposito <esposito_andrea99@hotmail.com>
2020-02-18 20:55:16 +0100

    Aggiunta possibilità di variare la dimensione del font
    Per inserirla si è rimosso il logo degli FSC dal footer. Si attendono eventuali istruzioni da parte del team @F-S-C/emotionally per reinserirlo.

--- src/resources/views/layouts/master.blade.php
+++ src/resources/views/layouts/master.blade.php
@@ -29,16 +29,81 @@
                         <a href="https://F-S-C.github.io/" rel="noopener noreferrer" target="_blank">FSC</a>.
                         @lang('metadata.copyright')
                     </p>
-                    <img src="{{asset('/fsc_logo_text.png')}}" width="140" alt="Five Students of Computer Science"
-                         class="float-md-right d-md-inline-block mr-md-3 d-none">
-                    <img src="{{asset('/fsc_logo.svg')}}" width="35" alt="Five Students of Computer Science"
-                         class="float-sm-right float-none d-md-none d-sm-inline-block d-none">
+                    <div class="float-right">
+                        <span>@lang('accessibility.font_size')</span>
+                        <button id="decrease-font-size" class="font-size-button btn btn-md-text">
+                            <span class="sr-only">@lang('accessibility.decrease_font_size')</span>
+                            <span class="fas fa-minus" aria-hidden="true"
+                                  title="@lang('accessibility.decrease_font_size')"></span>
+                        </button>
+                        <span aria-label="@lang('accessibility.current_font_size')" id="current-font-size">M</span>
+                        <button id="increase-font-size" class="font-size-button btn btn-md-text">
+                            <span class="sr-only">@lang('accessibility.increase_font_size')</span>
+                            <span class="fas fa-plus" aria-hidden="true"
+                                  title="@lang('accessibility.increase_font_size')"></span>
+                        </button>
+                    </div>
+{{--                    <img src="{{asset('/fsc_logo_text.png')}}" width="140" alt="Five Students of Computer Science"--}}
+{{--                         class="float-md-right d-md-inline-block mr-md-3 d-none">--}}
+{{--                    <img src="{{asset('/fsc_logo.svg')}}" width="35" alt="Five Students of Computer Science"--}}
+{{--                         class="float-sm-right float-none d-md-none d-sm-inline-block d-none">--}}
                 </div>
             </div>
         @show
     </footer>
 
     <script src="{{mix('/js/app.js')}}" type="text/javascript"></script>
+
+    <script>
+        (function () {
+            const DEFAULT_FONT_SIZE = 2;
+            let fontSizes = ['XS', 'S', 'M', 'L', 'XL'];
+            let currentFontSize = window.localStorage.getItem('font-size') || DEFAULT_FONT_SIZE;
+            let decreaseFontSizeButton = $('#decrease-font-size');
+            let increaseFontSizeButton = $('#increase-font-size');
+            let currentFontSizeIndicator = $('#current-font-size');
+            let documentRoot = $('html');
+
+            function getFontSizeClassName(fontSize) {
+                return 'font-size-' + fontSizes[fontSize].toLowerCase()
+            }
+
+            function limitCurrentFontSize() {
+                if (currentFontSize <= 0) {
+                    currentFontSize = 0;
+                    decreaseFontSizeButton.attr('disabled', true);
+                } else if (currentFontSize >= fontSizes.length - 1) {
+                    currentFontSize = fontSizes.length - 1;
+                    increaseFontSizeButton.attr('disabled', true);
+                }
+            }
+
+            function resizeFont(previousFontSize = null) {
+                currentFontSizeIndicator.text(fontSizes[currentFontSize]);
+                if (previousFontSize !== null) documentRoot.removeClass(getFontSizeClassName(previousFontSize));
+                documentRoot.addClass(getFontSizeClassName(currentFontSize));
+
+                window.localStorage.setItem('font-size', currentFontSize);
+            }
+
+
+            resizeFont();
+            limitCurrentFontSize();
+
+            decreaseFontSizeButton.click(function () {
+                currentFontSize--;
+                limitCurrentFontSize();
+                increaseFontSizeButton.attr('disabled', false);
+                resizeFont(currentFontSize + 1);
+            });
+            increaseFontSizeButton.click(function () {
+                currentFontSize++;
+                limitCurrentFontSize();
+                decreaseFontSizeButton.attr('disabled', false);
+                resizeFont(currentFontSize - 1);
+            });
+        })();
+    </script>
 
     @yield('scripts', '')
 </body>
