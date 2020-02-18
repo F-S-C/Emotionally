fd32a81036ce49b991a350bc2e9051dfaaefbee7
Graziano <graziano.montanaro98@gmail.com>
2020-02-18 19:36:39 +0100

    Inserito bottone dropdown per gestione permessi

--- src/resources/views/shared/dropdown-options-project.blade.php
+++ src/resources/views/shared/dropdown-options-project.blade.php
@@ -13,6 +13,8 @@
     </button>
     {{-- TODO: Edit options --}}
     <div class="dropdown-menu" aria-labelledby="more-project-{{$project->id}}">
+        <button class="dropdown-iten btn btn-link permission-project-btn">Permissions</button>
+        <div class="dropdown-divider"></div>
         <button class="dropdown-item btn btn-link rename-project-btn">Rename</button>
         @if($project->father_project != "")
         <button class="dropdown-item btn btn-link move-project-btn">Move</button>
